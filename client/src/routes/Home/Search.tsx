a7a262ffdb01f203699ee8d0bf03aca50fdbe55d
cbonoz <chrisdistrict@gmail.com>
2019-12-29 19:54:13 -0500

    placeholder

--- client/src/routes/Home/Search.tsx
+++ client/src/routes/Home/Search.tsx
@@ -1,51 +1,46 @@
-import React, { ChangeEvent, FormEvent, PureComponent } from 'react'
-import Button from '../../components/atoms/Button'
-import Form from '../../components/atoms/Form/Form'
-import Input from '../../components/atoms/Form/Input'
+import React, { ChangeEvent, FormEvent, PureComponent } from 'react';
+import Button from '../../components/atoms/Button';
+import Form from '../../components/atoms/Form/Form';
+import Input from '../../components/atoms/Form/Input';
 
 interface SearchProps {
-    searchAssets: any
+	searchAssets: any;
 }
 
 interface SearchState {
-    search: string
+	search: string;
 }
 
 export default class Search extends PureComponent<SearchProps, SearchState> {
-    public state = {
-        search: ''
-    }
+	public state = {
+		search: ''
+	};
 
-    private inputChange = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({
-            search: event.target.value
-        })
-    }
+	private inputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		this.setState({
+			search: event.target.value
+		});
+	};
 
-    public render() {
-        const { search } = this.state
+	public render() {
+		const { search } = this.state;
 
-        return (
-            <Form
-                onSubmit={(e: FormEvent<HTMLFormElement>) =>
-                    this.props.searchAssets(search, e)
-                }
-                minimal
-            >
-                <Input
-                    type="search"
-                    name="search"
-                    label="Search for data sets"
-                    placeholder="e.g. shapes of plants"
-                    value={search}
-                    onChange={this.inputChange}
-                    group={
-                        <Button primary disabled={!search}>
-                            Search
-                        </Button>
-                    }
-                />
-            </Form>
-        )
-    }
+		return (
+			<Form onSubmit={(e: FormEvent<HTMLFormElement>) => this.props.searchAssets(search, e)} minimal>
+				<Input
+					type="search"
+					name="search"
+					label="Search for data sets"
+					placeholder="e.g. Data mining in healthcare"
+					value={search}
+					onChange={this.inputChange}
+					group={
+						<Button primary disabled={!search}>
+							Search
+						</Button>
+					}
+				/>
+			</Form>
+		);
+	}
 }
