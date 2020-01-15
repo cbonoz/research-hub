c530856be62cd7332b4311fa86649e18b8baacca
cbonoz <chrisdistrict@gmail.com>
2020-01-14 21:57:27 -0500

    colors and base url

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 const config = {
-    app: { port: 4000 },
-    sendgridApiKey: process.env.SENDGRID_API_KEY,
-    ipfsGatewayUri: process.env.IPFS_GATEWAY_URI || 'https://gateway.ipfs.io'
-}
+	app: { port: 4000 },
+	sendgridApiKey: process.env.SENDGRID_API_KEY,
+	ipfsGatewayUri: process.env.IPFS_GATEWAY_URI || 'https://gateway.ipfs.io'
+};
 
-export default config
+export default config;
